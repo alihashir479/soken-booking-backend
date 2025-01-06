7b297ff58e0ce50b25842804b285150f8a78ec51
Hashir <alihashir479@gmail.com>
2025-01-06 08:01:06 +0500

    Edit my hotel and small fixes

--- src/controllers/Hotel.ts
+++ src/controllers/Hotel.ts
@@ -7,14 +7,7 @@ const createHotel = async (req: Request, res: Response) => {
     const images = req.files as Express.Multer.File[];
     const userId = req.userId;
 
-    const imagePromises = images.map(async (image) => {
-      const base64String = Buffer.from(image.buffer).toString("base64");
-      const base64Image = `data:${image.mimetype};base64,${base64String}`;
-      const imageUrl = await cloudinary.v2.uploader.upload(base64Image);
-      return imageUrl.url;
-    });
-
-    const imageUrls = await Promise.all(imagePromises);
+    const imageUrls = await uploadImagesToCloudinary(images);
     const newHotel = new Hotel(req.body);
 
     newHotel.imageUrls = imageUrls;
@@ -40,4 +33,73 @@ const fetchHotels = async (req: Request, res: Response) => {
   }
 };
 
-export { createHotel, fetchHotels };
+const fetchHotelById = async (req: Request, res: Response) => {
+  try {
+    const { id: hotelId } = req.params;
+    const hotel = await Hotel.findOne({ _id: hotelId, userId: req.userId });
+    if (!hotel) {
+      res.status(404).json({ message: "No hotel found" });
+      return;
+    }
+    res.json(hotel);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+};
+
+const updateHotel = async (req: Request, res: Response) => {
+  try {
+    const { id:hotelId } = req.params
+    const images = req.files as Express.Multer.File[]
+
+    const hotel = await Hotel.findById(hotelId)
+
+    if(!hotel) {
+      res.status(404).json({ message: 'No hotel found'})
+      return
+    }
+
+    if(hotel.userId.toString() !== req.userId.toString()) {
+      res.status(401).json({ message: 'Unauthorized'})
+      return
+    }
+
+    const imageUrls = await uploadImagesToCloudinary(images)
+
+    hotel.lastUpdated = new Date()
+    hotel.imageUrls = [...imageUrls, ...(req.body.imageUrls || [])]
+
+    // Fill other data
+    hotel.name = req.body.name
+    hotel.city = req.body.city
+    hotel.country = req.body.country
+    hotel.description = req.body.description
+    hotel.type = req.body.type
+    hotel.adultCount = req.body.adultCount
+    hotel.childCount = req.body.childCount
+    hotel.pricePerNight = req.body.pricePerNight
+    hotel.starRating = req.body.starRating
+    hotel.facilities = req.body.facilities
+
+    await hotel.save()
+    res.status(201).json(hotel)
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error updating hotel" });
+  }
+};
+
+const uploadImagesToCloudinary = async (images: Express.Multer.File[]) => {
+  const imagePromises = images.map(async (image) => {
+    const base64String = Buffer.from(image.buffer).toString("base64");
+    const base64Image = `data:${image.mimetype};base64,${base64String}`;
+    const imageUrl = await cloudinary.v2.uploader.upload(base64Image);
+    return imageUrl.url;
+  });
+
+  return await Promise.all(imagePromises);
+};
+
+export { createHotel, fetchHotels, fetchHotelById, updateHotel };
--- src/routes/Hotel.ts
+++ src/routes/Hotel.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { verifyToken } from "../middlware/VerifyToken";
 import { hotelRegistrationValidation } from "../middlware/validation";
 import multer from "multer";
-import { createHotel, fetchHotels } from "../controllers/Hotel";
+import { createHotel, fetchHotelById, fetchHotels, updateHotel } from "../controllers/Hotel";
 
 const MAX_IMAGES_ALLOWED_PER_HOTEL = 6
 
@@ -16,6 +16,8 @@ const upload = multer({
 
 const router = express.Router();
 router.post("/", upload.array('imageFiles', MAX_IMAGES_ALLOWED_PER_HOTEL), hotelRegistrationValidation, verifyToken, createHotel);
+router.put("/:id", verifyToken, upload.array('imageFiles', MAX_IMAGES_ALLOWED_PER_HOTEL), hotelRegistrationValidation, updateHotel);
 router.get('/', verifyToken, fetchHotels)
+router.get('/:id', verifyToken, fetchHotelById)
 
 export default router;
